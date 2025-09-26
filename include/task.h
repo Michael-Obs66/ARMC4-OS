12ce90af4a50c4602e5fd37e413319bacdc5f56a
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-26 23:52:39 +0700

    Update task.h

--- include/task.h
+++ include/task.h
@@ -1,11 +1,8 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include "kernel.h"   // biar task_t dikenali
 #include <stdint.h>
-
-typedef struct {
-    uint32_t r0, r1, r2, r3, r12, lr, pc, xpsr;
-} context_t;
 
 void task_init(void);
 task_t* task_create(void (*func)(void*), void *arg, uint32_t priority);
@@ -14,3 +11,5 @@ void task_resume(uint32_t task_id);
 void task_yield(void);
 
 #endif
+
+
