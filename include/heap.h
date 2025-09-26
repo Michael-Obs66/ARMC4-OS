5cf16e4446c86f73786106e82b32729898e85d5c
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-27 01:42:23 +0700

    Create heap.h

---
+++ include/heap.h
@@ -0,0 +1,20 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <stdint.h>
+
+// Heap management functions
+void heap_init(void);
+void *heap_alloc(uint32_t size);
+void heap_free(void *ptr);
+uint32_t heap_get_free_size(void);
+uint32_t heap_get_used_size(void);
+
+// Heap block structure
+typedef struct heap_block {
+    uint32_t size : 31;
+    uint32_t used : 1;
+    struct heap_block *next;
+} heap_block_t;
+
+#endif
