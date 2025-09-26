955e266acf5b1051a651d4d13c0ea4db4fa5bb06
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-27 03:44:01 +0700

    Update heap.c

--- mm/heap.c
+++ mm/heap.c
@@ -1,58 +1,49 @@
-#include <stddef.h>
 #include "../include/heap.h"
 
-#define HEAP_BLOCK_SIZE 32
-#define HEAP_BLOCKS (HEAP_SIZE / HEAP_BLOCK_SIZE)
-
-typedef struct heap_block {
-    uint32_t size : 31;
-    uint32_t used : 1;
-} heap_block_t;
+#define HEAP_SIZE       (1024 * 10) // 10 KB heap
+#define HEAP_BLOCK_SIZE 32           // ukuran tiap block
+#define HEAP_BLOCKS     (HEAP_SIZE / HEAP_BLOCK_SIZE)
 
 static heap_block_t heap_blocks[HEAP_BLOCKS];
 
-void heap_init(void)
-{
+void heap_init(void) {
     for (int i = 0; i < HEAP_BLOCKS; i++) {
-        heap_blocks[i].used = 0;
         heap_blocks[i].size = HEAP_BLOCK_SIZE;
+        heap_blocks[i].used = 0;
+        heap_blocks[i].next = (i < HEAP_BLOCKS - 1) ? &heap_blocks[i + 1] : NULL;
     }
 }
 
-void *heap_alloc(uint32_t size)
-{
-    uint32_t blocks_needed = (size + sizeof(heap_block_t) + HEAP_BLOCK_SIZE - 1) / HEAP_BLOCK_SIZE;
-    
-    for (int i = 0; i < HEAP_BLOCKS - blocks_needed; i++) {
-        if (!heap_blocks[i].used) {
-            int consecutive_free = 0;
-            for (int j = i; j < i + blocks_needed; j++) {
-                if (!heap_blocks[j].used) {
-                    consecutive_free++;
-                }
-            }
-            
-            if (consecutive_free == blocks_needed) {
-                heap_blocks[i].used = 1;
-                heap_blocks[i].size = blocks_needed * HEAP_BLOCK_SIZE;
-                return (void*)((uint8_t*)heap_blocks + i * HEAP_BLOCK_SIZE + sizeof(heap_block_t));
-            }
+void *heap_alloc(uint32_t size) {
+    // implementasi sederhana
+    heap_block_t *block = heap_blocks;
+    while (block) {
+        if (!block->used && block->size >= size) {
+            block->used = 1;
+            return (void *)(block + 1); // return alamat setelah header
         }
+        block = block->next;
     }
-    
-    return NULL;
+    return NULL; // gagal alokasi
 }
 
-void heap_free(void *ptr)
-{
-    if (ptr == NULL) return;
-    
-    uint32_t block_index = ((uint8_t*)ptr - (uint8_t*)heap_blocks - sizeof(heap_block_t)) / HEAP_BLOCK_SIZE;
-    
-    if (block_index < HEAP_BLOCKS) {
-        heap_blocks[block_index].used = 0;
-    }
+void heap_free(void *ptr) {
+    if (!ptr) return;
+    heap_block_t *block = ((heap_block_t *)ptr) - 1;
+    block->used = 0;
 }
 
+uint32_t heap_get_free_size(void) {
+    uint32_t free_size = 0;
+    heap_block_t *block = heap_blocks;
+    while (block) {
+        if (!block->used)
+            free_size += block->size;
+        block = block->next;
+    }
+    return free_size;
+}
 
-
+uint32_t heap_get_used_size(void) {
+    return HEAP_SIZE - heap_get_free_size();
+}
