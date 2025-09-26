b27d8340977cc219353f695346819ab6a6003490
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-27 00:21:58 +0700

    Update mm.c

--- mm/mm.c
+++ mm/mm.c
@@ -1,59 +1,40 @@
-#include <stdint.h>
-#include <stddef.h>
+#include "mm.h"
+#include "../lib/printf.h"
 
-#define HEAP_START 0x20001000
-#define HEAP_END   0x2000F000
-#define BLOCK_SIZE 64
+#define HEAP_SIZE (64 * 1024) // 64KB
+static uint8_t heap[HEAP_SIZE];
+static uint32_t heap_used = 0;
 
-typedef struct block {
-    struct block *next;
-    size_t size;
-    int free;
-} block_t;
-
-static block_t *free_list = NULL;
-
-void mm_init(void) {
-    free_list = (block_t*)HEAP_START;
-    free_list->size = HEAP_END - HEAP_START - sizeof(block_t);
-    free_list->free = 1;
-    free_list->next = NULL;
+void mm_init(void)
+{
+    heap_used = 0;
+    printf("Memory manager initialized. Heap size: %d bytes\r\n", HEAP_SIZE);
 }
 
-void* kmalloc(size_t size) {
-    block_t *current = free_list;
-    block_t *previous = NULL;
-    
-    while (current) {
-        if (current->free && current->size >= size) {
-            if (current->size > size + sizeof(block_t) + BLOCK_SIZE) {
-                // Split block
-                block_t *new_block = (block_t*)((char*)current + sizeof(block_t) + size);
-                new_block->size = current->size - size - sizeof(block_t);
-                new_block->free = 1;
-                new_block->next = current->next;
-                
-                current->size = size;
-                current->next = new_block;
-            }
-            current->free = 0;
-            return (void*)((char*)current + sizeof(block_t));
-        }
-        previous = current;
-        current = current->next;
+void *kmalloc(uint32_t size)
+{
+    // Simple sequential allocator
+    if (heap_used + size > HEAP_SIZE) {
+        return NULL;
     }
-    return NULL;
+    
+    void *ptr = &heap[heap_used];
+    heap_used += size;
+    return ptr;
 }
 
-void kfree(void *ptr) {
-    if (!ptr) return;
-    
-    block_t *block = (block_t*)((char*)ptr - sizeof(block_t));
-    block->free = 1;
-    
-    // Merge with next block if free
-    if (block->next && block->next->free) {
-        block->size += sizeof(block_t) + block->next->size;
-        block->next = block->next->next;
-    }
+void kfree(void *ptr)
+{
+    // Simple allocator doesn't support free
+    // In real implementation, this would manage free blocks
+}
+
+uint32_t mm_get_free(void)
+{
+    return HEAP_SIZE - heap_used;
+}
+
+uint32_t mm_get_used(void)
+{
+    return heap_used;
 }
