670626cdc8926a55871b59fdd5ffc3e44bdb4ad2
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-27 00:04:14 +0700

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -2,11 +2,18 @@
 #define KERNEL_H
 
 #include <stdint.h>
+#include "task.h"
 
-// Inisialisasi kernel
 void kernel_init(void);
+void kernel_start(void);
+void create_task(void (*task)(void*), void *arg, uint32_t priority);
+void scheduler(void);
+void context_switch(void);
+void systick_handler(void);
 
-// Buat task baru (stub)
-void create_task(void (*task_func)(void*), const char *name);
+extern task_t *current_task;
+extern task_t task_table[MAX_TASKS];
+extern uint32_t task_count;
 
-#endif // KERNEL_H
+#endif
+
