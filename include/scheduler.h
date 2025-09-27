f33e2d77d3e5146d4dc6f55909c158c168d710b1
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-27 13:52:29 +0700

    Update scheduler.h

--- include/scheduler.h
+++ include/scheduler.h
@@ -1,8 +1,11 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include <stdint.h>
 #include "task.h"
 
+
+extern task_t *current_task;  
 void scheduler_init(void);
 void scheduler_add_task(task_t *task);
 void scheduler_remove_task(task_t *task);
