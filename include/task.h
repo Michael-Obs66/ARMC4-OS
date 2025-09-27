33a9b09c5dac7b35d7c1d5cb8f9c46d2daaea04b
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-09-27 13:36:32 +0700

    Update task.h

--- include/task.h
+++ include/task.h
@@ -3,40 +3,40 @@
 
 #include <stdint.h>
 
-#define MAX_TASKS 16
-#define TASK_STACK_SIZE 1024
-#define TASK_TIME_SLICE 10
+// Konstanta untuk task
+#define MAX_TASKS        16
+#define TASK_STACK_SIZE  256
+#define TASK_TIME_SLICE  10
 
+// Status task
 typedef enum {
-    TASK_FREE,
+    TASK_FREE = 0,
     TASK_READY,
     TASK_RUNNING,
-    TASK_BLOCKED,
-    TASK_SLEEPING
+    TASK_SLEEPING,
+    TASK_ZOMBIE        
 } task_state_t;
 
+// Struktur task
 typedef struct task {
     uint32_t pid;
-    uint32_t *sp;
-    uint32_t *stack_ptr;
+    uint8_t  priority;
     task_state_t state;
-    uint8_t priority;
+    uint32_t *sp;               
+    uint32_t *stack_ptr;        
     uint32_t time_slice;
     uint32_t sleep_ticks;
-    struct task *next;
-    struct task *prev;
 } task_t;
 
-// Task management
+
+extern task_t task_table[MAX_TASKS];
+
+
 void task_init(void);
-task_t *task_create(void (*entry)(void*), void *arg, uint8_t priority);
+task_t *task_create(void (*entry)(void *), void *arg, uint8_t priority);
 void task_exit(void);
 void task_sleep(uint32_t ticks);
 void task_wakeup_sleeping(void);
 task_t *task_get_by_pid(uint32_t pid);
-
-// **Task switching functions required by scheduler**
-void task_switch(task_t *prev, task_t *next);
-void task_switch_to(task_t *next);
 
 #endif
