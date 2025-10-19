24f8e2c72ecbf7c28888b10f45c8e3227984ac57
Michael Anggi Gilang Angkasa <63223847+Michael-Obs66@users.noreply.github.com>
2025-10-19 18:03:28 +0700

    Update startup_stm32f446re.s

--- boards/nucleo-f446re/startup_stm32f446re.s
+++ boards/nucleo-f446re/startup_stm32f446re.s
@@ -1,33 +1,44 @@
 .syntax unified
 .cpu cortex-m4
-.fpu softvfp
 .thumb
 
-.global g_pfnVectors
+.global _estack
 .global Reset_Handler
 
+/* =========================================================
+ * Vector Table
+ * ========================================================= */
 .section .isr_vector, "a", %progbits
+.type g_pfnVectors, %object
+.size g_pfnVectors, .-g_pfnVectors
 g_pfnVectors:
-  .word _estack
-  .word Reset_Handler
-  .word NMI_Handler
-  .word HardFault_Handler
-  .word MemManage_Handler
-  .word BusFault_Handler
-  .word UsageFault_Handler
-  .word 0
-  .word 0
-  .word 0
-  .word 0
-  .word SVC_Handler
-  .word DebugMon_Handler
-  .word 0
-  .word PendSV_Handler
-  .word SysTick_Handler
+    .word _estack
+    .word Reset_Handler
+    .word NMI_Handler
+    .word HardFault_Handler
+    .word MemManage_Handler
+    .word BusFault_Handler
+    .word UsageFault_Handler
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word SVC_Handler
+    .word DebugMon_Handler
+    .word 0
+    .word PendSV_Handler
+    .word SysTick_Handler
 
+/* =========================================================
+ * Reset Handler
+ * ========================================================= */
 .section .text.Reset_Handler
 Reset_Handler:
-  ldr sp, =_estack
-  bl SystemInit
-  bl main
-  b .
+    ldr r0, =_estack
+    mov sp, r0
+
+    bl SystemInit
+    bl main
+
+LoopForever:
+    b LoopForever
